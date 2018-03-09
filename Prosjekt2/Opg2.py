393889bcb88068e533534c2613f800d125668274
helemo <35994246+helemo@users.noreply.github.com>
2018-03-09 15:51:09 +0100

    Update Opg2.py

--- Prosjekt2/Opg2.py
+++ Prosjekt2/Opg2.py
@@ -1,57 +1,15 @@
-from Vitber.Prosjekt2.Opg1 import findX
 import scipy
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import timeit
 
-from Vitber.Prosjekt2.ny import makeGrid
-
-'''
-Define a straight polymer consisting of 15 monomers. Follow the example in Sec. 1.4, and plot the mean energy, ⟨E⟩,
-as function of temperature. Choose the relevant parameters as in Sec. 1.3. That is, choose Uij randomly
-between −3.47· 10−21 and −10.4· 10−21 (in units of J), and vary T between 0 K and 1500 K. You need to do d(T)
-twists at each temperature T, where d(T) = dmax exp{−sT}, as explained in Sec. 1.4. Choose dmax and s so that you
-obtain a sufficiently convergent5 result,.
-'''
-
-
-# Lag polymermatrise
-
-'''
-Denote the potential be- tween the amino acids i and j at positions ri and rj as U(ri − rj), 
-and the expression for the mean energy would then follow as ⟨E⟩ = sum (states (protein structure)) = sum U(ri − rj)
- That is, if A1 is nearest neighbour to A4, a weak bond forms between these two monomers
-'''
-
-# Lag nabomatrise U, med tall tilfeldig på intervall  (−3.47· 10−21 ,   −10.4· 10−21) (in units of J). (we define Ui j to be zero if |i − j| ≤ 1)
-# Polymer og nabo må kobles sammen slik at hvis twist så må nabo endres.
-
-def getEnergy(polymer, U): #finner energien til hele polymeret
-    totalEnergy = 0
-    for x in range (1,16): #Går igjennom lengden på polymeret
-        row, col = findX(polymer,x )
-        NeighbourEnergy = nearestNeighbours()
-        totalEnergy += NeighbourEnergy
-    return totalEnergy
-
-
-def nearestNeighbours(grid, U, row, col ):
-    n = len(grid)
-    if row + 1 < n:
-        E =+ U[grid[row + 1, col], grid[row, col]]
-    if row - 1 >= 0:
-        E = E + U[grid[row - 1, col], grid[row, col]]
-    if col + 1 < n:
-        E = E + U[grid[row, col + 1], grid[row, col]]
-    if col -1 >= 0:
-        E = E + U[grid[row, col - 1], grid[row, col]]
-    return E
-# dobbel for løkke, ytre løkke der T går fra 0 til 1500 K, og indreløkke kjører d(T) twists ved hver temp.
-
+from ny import twist_execute
+from ny import findX
+from ny import makeGrid
 
 def U_ij(lengde):
     U_ij = np.zeros((lengde+1, lengde+1))
-    print(U_ij)
 
     for i in range (1,(lengde+1)):
          for j in range (1,lengde +1):
@@ -66,3 +24,83 @@ def U_ij(lengde):
 
     return U_ij
 
+def nearestNeighbours(grid, U, row, col ):
+    n = len(grid)
+    if row + 1 < n:
+        E =+ U[grid[row + 1, col], grid[row, col]]
+    if row - 1 >= 0:
+        E = E + U[grid[row - 1, col], grid[row, col]]
+    if col + 1 < n:
+        E = E + U[grid[row, col + 1], grid[row, col]]
+    if col -1 >= 0:
+        E = E + U[grid[row, col - 1], grid[row, col]]
+    return E
+# dobbel for løkke, ytre løkke der T går fra 0 til 1500 K, og indreløkke kjører d(T) twists ved hver temp.
+
+def getEnergy(polymer, U): #finner energien til hele polymeret
+    totalEnergy = 0
+    for x in range (1,16): #Går igjennom lengden på polymeret
+        row, col = findX(polymer,x )
+        NeighbourEnergy = nearestNeighbours(polymer,U,row,col)
+        totalEnergy += NeighbourEnergy
+    return totalEnergy
+
+def plotEnergy(grid):
+    start = timeit.timeit()
+
+    U = U_ij(15)
+    E = np.zeros(1500)
+    Temp = np.zeros(1500)
+
+    for T in range (1,1500,1):
+        antall_twists = np.floor(11000 * np.exp(-0.0015 * (T-0.9999999))).astype(int)
+        polymer = twist_execute(antall_twists,15, grid)
+        E = np.append(E,(getEnergy(polymer,U)))
+        Temp = np.append(Temp, (T-0.9999999))
+        print(T)
+
+
+    plt.plot(Temp, E)
+    plt.show()
+
+    end = timeit.timeit()
+    print(end-start)
+
+def plotBindingEnergy(grid):
+    start = timeit.timeit()
+
+    U = U_ij(15)
+    E = np.zeros(5000)
+    Twists = np.zeros(5000)
+
+    for Twist in range(1, 5000):
+        polymer = twist_execute(Twist, 15, grid)
+        E = np.append(E, (getEnergy(polymer, U)))
+        Twists = np.append(Twists, Twist)
+        print(Twist)
+
+    plt.plot(Twists, E)
+    plt.show()
+
+    end = timeit.timeit()
+    print(end - start)
+
+
+
+def main():
+    polymer = makeGrid(15)
+    plotEnergy(polymer)
+    plotBindingEnergy(polymer)
+
+    '''#antall_twists = np.floor(11000 * np.exp(-0.0015 * 10)).astype(int)
+    antall_twists = 5
+    grid = twist_execute(antall_twists, 15)
+    print(grid)
+    U = U_ij(15)
+    print(U)
+    energy = getEnergy(grid,U)
+    print(energy)'''
+
+
+
+main()

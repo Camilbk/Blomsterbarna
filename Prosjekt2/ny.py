ee99c8a7ceaabca965d9e21355c69ddbc79f022a
helemo <35994246+helemo@users.noreply.github.com>
2018-03-14 10:57:40 +0100

    Update ny.py

--- Prosjekt2/ny.py
+++ Prosjekt2/ny.py
@@ -2,9 +2,11 @@ import scipy
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import timeit
+import random
 
-from energy import getEnergy
-from energy import U_ij
+#from energy import getEnergy
+#from energy import U_ij
 
 def makeGrid(N):  # nxn matrise
     grid = np.zeros((N+2, N+2)).astype(np.int16)
@@ -50,10 +52,13 @@ def twist(grid, lengde,T):
     B = 1 / (kb * T)
 
     while(twist == False):
-        rot, rigid = rigid_rot(grid, x, lengde)
+        x = random.randint(1,lengde + 1)
+        print(x, 'x')
 
-        x = np.random.randint(1, high=lengde + 1)
-        n = np.random.randint(2, size=1)  # clockwise
+        n = np.random.randint(2, size=None) #genererer clockwise
+        print(n, 'n')
+
+        rot, rigid = rigid_rot(grid, x, lengde)
 
         row_nonzero = np.count_nonzero(np.count_nonzero(rot, axis=1)) #teller rader med tall i
         col_nonzero = np.count_nonzero(np.count_nonzero(rot, axis=0)) #teller kolonner med tall i
@@ -85,28 +90,101 @@ def twist(grid, lengde,T):
     else:
         return grid
 
-def twist_execute(antall_twists,lengde,twisted_matrix, T):
+def twist_execute(antall_twists,lengde,grid, T):
     for i in range(np.round(antall_twists)):
+        print('løkken har gjørt ', i, 'ganger')
+        temp = grid
+        twisted_matrix = twist(temp, lengde, T)
         grid = twisted_matrix
-        polymer = twist(grid, lengde, T)
-        twisted_matrix = polymer
 
-    return twisted_matrix
+    return grid
+################ OPPGAVE 2 ###############
+
+def U_ij(lengde):
+    U_ij = np.zeros((lengde+1, lengde+1))
+
+    for i in range (1,(lengde+1)):
+         for j in range (1,lengde +1):
+            np.set_printoptions(precision=2)
+            U_ij[i,j] = np.random.uniform(low = (-3.47*10**(-21)), high= (-10.4*10**(-21)))
+            np.fill_diagonal(U_ij,0)
+
+    for z in range (1,lengde):
+            U_ij[z,z] = 0
+            U_ij[z+1,z] = 0
+            U_ij[z,z+1] = 0
+
+    return U_ij
+
+def nearestNeighbours(grid, U, row, col ):
+    n = len(grid)
+    if row + 1 < n:
+        E =+ U[grid[row + 1, col], grid[row, col]]
+    if row - 1 >= 0:
+        E = E + U[grid[row - 1, col], grid[row, col]]
+    if col + 1 < n:
+        E = E + U[grid[row, col + 1], grid[row, col]]
+    if col -1 >= 0:
+        E = E + U[grid[row, col - 1], grid[row, col]]
+    return E
+# dobbel for løkke, ytre løkke der T går fra 0 til 1500 K, og indreløkke kjører d(T) twists ved hver temp.
+
+def getEnergy(polymer, U): #finner energien til hele polymeret
+    totalEnergy = 0
+    for x in range (1,16): #Går igjennom lengden på polymeret
+        row, col = findX(polymer,x )
+        NeighbourEnergy = nearestNeighbours(polymer,U,row,col)
+        totalEnergy += NeighbourEnergy
+    return totalEnergy
+
+def plotEnergy(grid):
+    start = timeit.timeit()
+
+    U = U_ij(15)
+    E = np.zeros(1500)
+    Temp = np.zeros(1500)
+
+    for T in range (1,1500,1):
+        antall_twists = np.floor(11000 * np.exp(-0.0015 * (T-0.9999999))).astype(int)
+        polymer = twist_execute(antall_twists,15, grid)
+        E = np.append(E,(getEnergy(polymer,U)))
+        Temp = np.append(Temp, (T-0.9999999))
+        print(T)
 
 
+    plt.plot(Temp, E)
+    plt.show()
+
+    end = timeit.timeit()
+    print(end-start)
+
+def plotBindingEnergy(grid):
+    start = timeit.timeit()
+
+    U = U_ij(15)
+    E = np.zeros(5000)
+    Twists = np.zeros(5000)
+
+    for Twist in range(1, 5000):
+        polymer = twist_execute(Twist, 15, grid)
+        E = np.append(E, (getEnergy(polymer, U)))
+        Twists = np.append(Twists, Twist)
+        print(Twist)
+
+    plt.plot(Twists, E)
+    plt.show()
+
+    end = timeit.timeit()
+    print(end - start)
 
 
 def main():
     polymer = makeGrid(15)
-    twist(polymer,15,500)
+    print(polymer)
+    print(twist(polymer,15,500))
+    print(twist_execute(2,polymer,15,500))
 
-
-
-
-
-
-
-
+main()
 
 
 
